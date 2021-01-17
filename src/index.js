5e8db55451a4af9975e7d1b8abdb74ac414053a3
Imtiaz Uddin <imtiaz100463@gmail.com>
2021-01-17 14:56:55 +0600

    demo application

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const routes = require('./routes')
+
+const app = express()
+
+const port = 5050;
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, '../','public')));
+
+routes(app);
+app.get('/', (req, res) => {
+	
+	res.send('Hello World!')
+});
+
+app.use((req, res, next) => {
+	const error = new Error('404 Not Found');
+	error.status = 404;
+	next(error);
+
+});
+
+app.use((error, req, res, next) => {
+	console.log(error)
+	if (error.status === 404) {
+		res.status(404);
+		return res.render('errors/404')
+	}
+	res.status(500);
+	res.render('errors/500')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
